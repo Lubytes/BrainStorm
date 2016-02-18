b09df4429a7b8fc2001554813c5428d24286cc54
ranranrunforit <1204019784@qq.com>
2016-02-18 16:40:25 -0400

    Footer, Header
    the Footer and Header for the every page

---
+++ footer.php
@@ -0,0 +1,8 @@
+    </div> <!-- /.container -->
+
+    <script src="jquery/jquery-1.11.2.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="ckeditor/ckeditor.js"></script>
+ 
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Brain Storm</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    
+    <style>
+      .container {
+        max-width: 800px;
+      }
+      textarea {
+        resize: vertical;
+      }
+    </style>
+
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-default">
+    <div class="container" style="max-width: 800px;">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="Login.php">Brain Storm. Inc</a>
+      </div>
+
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" >
+        </div>
+      </form>
+
+        <ul class="nav navbar-nav navbar-left">
+          <li><a href="Login.php">Welcome to the Storm</a></li>
+          <li><a href=" ">Look who's using it</a></li>
+        </ul>
+ 
+    </div>
+    </nav>
+
+    <div class="container">
+
+    <h1 class="text-center">Brain Storm. Inc</h1>
