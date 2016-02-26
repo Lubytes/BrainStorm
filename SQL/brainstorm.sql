df1bd1e946af756dd793f4b95b35cb42e354770a
johnalatouf <johnalatouf@gmail.com>
2016-02-26 17:59:16 -0400

    Added some things to project proposal and updated db export file
    I added an er diagram to the project proposal.
    The db now has tables for group creation.

--- SQL/brainstorm.sql
+++ SQL/brainstorm.sql
@@ -1,57 +1,78 @@
-CREATE DATABASE  IF NOT EXISTS `brainstorm` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `brainstorm`;
--- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.3.11.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1    Database: brainstorm
--- ------------------------------------------------------
--- Server version	5.5.34
+-- Host: db.cs.dal.ca
+-- Generation Time: Feb 26, 2016 at 03:57 PM
+-- Server version: 5.7.10
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `latouf`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `follows`
 --
 
-DROP TABLE IF EXISTS `follows`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `follows` (
-  `username` char(60) NOT NULL DEFAULT '',
-  `follower` char(60) NOT NULL DEFAULT '',
-  PRIMARY KEY (`username`,`follower`),
-  KEY `follower` (`follower`),
-  CONSTRAINT `follows_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`follower`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS `follows` (
+  `username` char(60) NOT NULL,
+  `follower` char(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `follows`
+-- Table structure for table `groups`
 --
 
-LOCK TABLES `follows` WRITE;
-/*!40000 ALTER TABLE `follows` DISABLE KEYS */;
-/*!40000 ALTER TABLE `follows` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `creator` char(60) DEFAULT NULL,
+  `groupID` int(11) NOT NULL,
+  `groupname` char(60) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`creator`, `groupID`, `groupname`, `description`) VALUES
+('admin', 1, 'EVERYONE', NULL),
+('admin', 2, 'PERSONAL', NULL),
+('admin', 3, 'EVERYONE', NULL),
+('admin', 4, 'PERSONAL', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `in_group`
+--
+
+CREATE TABLE IF NOT EXISTS `in_group` (
+  `username` char(60) NOT NULL,
+  `groupID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `posts`
 --
 
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `posts` (
-  `post_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_ID` int(11) NOT NULL,
   `head` int(11) DEFAULT NULL,
   `type` char(20) DEFAULT NULL,
   `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -59,59 +80,43 @@ CREATE TABLE `posts` (
   `image` varchar(255) DEFAULT NULL,
   `rating` int(11) DEFAULT NULL,
   `username` char(60) DEFAULT NULL,
-  PRIMARY KEY (`post_ID`),
-  KEY `username` (`username`),
-  KEY `head` (`head`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`head`) REFERENCES `posts` (`post_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `groupID` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts`
 --
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `posts` (`post_ID`, `head`, `type`, `date_time`, `content`, `image`, `rating`, `username`, `groupID`) VALUES
+(1, 1, 'empty', '2016-02-26 16:53:38', NULL, NULL, 0, 'admin', 1),
+(2, 1, 'idea', '2016-02-26 16:55:14', 'This is a head idea post because it''s head ID is 1.', NULL, 0, 'admin', 1),
+(3, 1, 'idea', '2016-02-26 16:56:02', 'This is a head idea post because it''s head ID is 1.', NULL, 0, 'admin', 1),
+(4, 1, 'idea', '2016-02-26 16:56:02', 'This is a head idea post because it''s head ID is 1.', NULL, 0, 'admin', NULL),
+(5, 3, 'branch', '2016-02-26 16:58:51', 'This is a sub-idea connected to postID 3.', NULL, 0, 'admin', NULL),
+(6, 3, 'branch', '2016-02-26 17:06:53', 'This is a sub-idea connected to postID 3.', NULL, 0, 'admin', NULL),
+(7, 2, 'branch', '2016-02-26 17:06:53', 'This is a sub-idea connected to postID 2.', NULL, 0, 'admin', NULL),
+(8, 6, 'branch', '2016-02-26 17:06:53', 'This is a sub-idea connected to postID 6.', NULL, 0, 'admin', NULL);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ratings`
 --
 
-DROP TABLE IF EXISTS `ratings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ratings` (
-  `username` char(60) NOT NULL DEFAULT '',
-  `post_ID` int(11) NOT NULL DEFAULT '0',
-  `value` int(11) DEFAULT NULL,
-  PRIMARY KEY (`post_ID`,`username`),
-  KEY `username` (`username`),
-  CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`post_ID`) REFERENCES `posts` (`post_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `username` char(60) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `value` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `ratings`
---
-
-LOCK TABLES `ratings` WRITE;
-/*!40000 ALTER TABLE `ratings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ratings` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `username` char(60) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` char(60) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `displayName` char(60) DEFAULT NULL,
@@ -119,27 +124,111 @@ CREATE TABLE `users` (
   `picture` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`username`)
+  `admin` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`username`, `password`, `email`, `displayName`, `gender`, `picture`, `description`, `status`, `admin`) VALUES
+('admin', '$6$rounds=5000$4Ds0.2.A.F*pPi(8$N6e1Efq/tuX7BXCy4d6VW4UOkZDUah7pRTUBjDKfUO6eq15IipMZWx.hmm12S1BuvZUQ.OFZZhbT98vuRJPfz/', '', 'admin', 'female', '', '', 0, 1),
+('Luonline', '$6$rounds=5000$4Ds0.2.A.F*pPi(8$l9H4aSNH3TppBLURj/LD0nCzkXMN/PZVvxx6h/ejbUODKTzG3OL6Qv../gf1yPUsVzSShHuALZSHIzdAfygM2/', '', 'Brandon', 'male', '', '', 0, 1),
+('ranran', '$6$rounds=5000$4Ds0.2.A.F*pPi(8$EhM4.anP6OiDX8pwP62m/L.W/4.Z8FJh9wUVKcCdrjtl0n.sVhjXkPCiYZJKreFEFf35ZwrBBvDEsqd.Tl1D3.', '', 'ranranrun', 'female', '', '', 0, 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `follows`
+--
+ALTER TABLE `follows`
+  ADD PRIMARY KEY (`username`,`follower`), ADD KEY `follower` (`follower`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`groupID`), ADD KEY `creator` (`creator`);
+
+--
+-- Indexes for table `in_group`
+--
+ALTER TABLE `in_group`
+  ADD PRIMARY KEY (`groupID`,`username`), ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_ID`), ADD KEY `username` (`username`), ADD KEY `head` (`head`), ADD KEY `groupID` (`groupID`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`post_ID`,`username`), ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `groupID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `follows`
+--
+ALTER TABLE `follows`
+ADD CONSTRAINT `follows_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`follower`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`username`);
+
+--
+-- Constraints for table `in_group`
+--
+ALTER TABLE `in_group`
+ADD CONSTRAINT `in_group_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`),
+ADD CONSTRAINT `in_group_ibfk_2` FOREIGN KEY (`groupID`) REFERENCES `groups` (`groupID`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`head`) REFERENCES `posts` (`post_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`groupID`) REFERENCES `groups` (`groupID`);
+
+--
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`post_ID`) REFERENCES `posts` (`post_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-02-18 12:46:57
